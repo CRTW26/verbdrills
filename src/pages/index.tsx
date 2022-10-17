4baac5a2de1e61aafbc184b9546709f2a9efcee7
CRTW26 <worthington.connor@gmail.com>
2022-10-17 19:53:24 +0100

    mod: add custom document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,23 +21,6 @@ export const Index: React.FC<Props> = () => {
     <>
       <Head>
         <title>verbdills</title>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
       </Head>
 
       <Layout>
